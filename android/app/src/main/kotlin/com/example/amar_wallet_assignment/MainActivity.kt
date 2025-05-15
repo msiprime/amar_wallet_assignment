99a894e0a1f72db7956cf2717d5b593df9058fbd
Md Sakibul Islam <msisakib958@gmail.com>
2025-05-15 22:46:22 +0600

    Update metadata revisions and set app title

--- android/app/src/main/kotlin/com/example/amar_wallet_assignment/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.amar_wallet_assignment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,12 +4,8 @@ import 'package:amar_wallet_assignment/features/base/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-/// The root widget of the application.
-///
-/// This widget sets up the BLoC providers, routing, theming, and localization
-/// for the entire app. It also handles connectivity status.
+
 class App extends StatefulWidget {
-  /// Creates a [App].
   const App({super.key});
 
   @override
@@ -27,6 +23,7 @@ class _AppState extends State<App> {
               ),
             ],
             child: MaterialApp(
+              title: '6amMart Delivery',
               theme: ThemeData.light(),
               home: SplashScreen(),
               debugShowCheckedModeBanner: false,
